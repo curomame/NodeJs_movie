8a7f26e57da2f67ecc2ca2f1f9e2beb8918afc38
HyangRim <curomame@naver.com>
2022-04-11 01:02:53 +0900

    til

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,7 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'e12ce854710de7d04be07f144c4f8943';
+
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FaCode } from "react-icons/fa";
+import {API_KEY, API_URL} from "../../Config"
 
 function LandingPage() {
+
+
+    useEffect(()=>{
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+
+        fetch(endpoint)
+        .then(res => res.json())
+        .then(res => console.log(res));
+
+    },[])
+
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+<div style={{ width : '100%', margin : '0'}}>
+    
+    {/* {Main Image} */}
+
+    <div style={{ width : '85%', margin : '1rem auto'}}>
+
+        <h2>Moveis by latest</h2>
+        <hr />
+
+        {/* {Movie Grid Cards} */}
+
+    </div>
+
+    <div style={{ display : 'flex', justifyContent : 'center'}}>
+        <button>Load Movie</button>
+    </div>
+
+</div>
     )
 }
 
