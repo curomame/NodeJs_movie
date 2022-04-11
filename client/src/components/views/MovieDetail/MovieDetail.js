fd160f1ab42655660f5cdfc43885077a9c965dc2
HyangRim <curomame@naver.com>
2022-04-11 23:43:29 +0900

    night til

--- client/src/components/views/MovieDetail/MovieDetail.js
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -2,13 +2,18 @@ import React, { useEffect, useState } from 'react'
 import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
 import MainImage from '../LandingPage/MainImage';
 import MovieInfo from './Section/MovieInfo';
+import GridCards from '../commoms/GridCards';
+import { Row, Button } from 'antd';
+import Favorite from './Section/Favorite';
+
 
 function MovieDetail(props) {
 
   let movieId = props.match.params.movieId;
 
   const [Movie, setMovie] = useState([]);
-
+  const [Casts, setCasts] = useState([]);
+  const [ActorToggle, setActorToggle] = useState(false);
 
   useEffect(() => {
 
@@ -22,9 +27,22 @@ function MovieDetail(props) {
         {
           setMovie(res);
         })
+
+      fetch(endpointCrew)
+      .then(res => res.json())
+      .then(res => 
+        {
+          setCasts(res.cast);
+        })
+
+        
       }
       
       ,[])
+
+
+      const toggleActorView = () =>  {setActorToggle(!ActorToggle)};
+
 
   return (
     <div>
@@ -38,6 +56,13 @@ function MovieDetail(props) {
 
       <div style={{ width : '85%', margin:'1rem auto'}}>
 
+
+      <div style={{display:'flex', justifyContent:'flex-end'}}>
+      
+        <Favorite movieInfo={Movie} movieId={movieId} userFrom={localStorage.getItem.userId}/>
+      
+      </div>
+
     <MovieInfo
     
       movie={Movie}
@@ -45,9 +70,36 @@ function MovieDetail(props) {
 
       <br/>
 
+
+
+
+
+
+
+
       <div style={{display:'flex', justifyContnet:'center', margin:'2rem'}}>
-        <button>Toggle Actor View</button>
+        <button onClick={toggleActorView}>Toggle Actor View</button>
       </div>
+
+
+
+      {ActorToggle && 
+            <Row gutter={[16,16]}>
+
+            {Casts && Casts.map((cast, index) => (
+                <React.Fragment key={index}>
+                    <GridCards 
+                        image={cast.profile_path ? 
+                        `${IMAGE_BASE_URL}w500${cast.profile_path}` : null}
+                        characterName={cast.name}
+                    />
+                </React.Fragment>
+            ))}
+        
+            
+        
+            </Row>
+            }
 
       </div>
     </div>
